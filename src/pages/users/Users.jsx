8460ae60e4b523bdc2e61ed175ae7f897e38419d
Arpit098 <arpitagrawal3048@outlook.com>
2023-11-17 00:32:58 +0530

    search user

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Users = () => {
-  return (
-    <div>users</div>
-  )
-}
+  const [userEmail, setUserEmail] = useState('');
+  const [userId, setUserId] = useState(null);
+  const [message, setMessage] = useState('');
 
-export default Users
+  const searchUser = async () => {
+    try {
+      const response = await axios.post('http://localhost:8000/search-user', { userEmail });
+      setUserId(response.data.userId);
+      setMessage(response.data.message);
+    } catch (error) {
+      console.error('Error searching user:', error);
+      setMessage('Error searching user');
+    }
+  };
+
+  return (
+    <div>
+      <label>
+        Enter user email:
+        <input type="text" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
+      </label>
+      <button onClick={searchUser}>Search User</button>
+      {userId && <p>User ID: {userId}</p>}
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default Users;
